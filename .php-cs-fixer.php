beaed1b60df74ef2346fae170d9f1b0bd336a0e0
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-02-01 07:50:27 +0800

    Optimize CI

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 /**
- * This file is part of huangdijia/laravel-horizon-restart.
+ * This file is part of laravel-horizon-restart.
  *
  * @link     https://github.com/huangdijia/laravel-horizon-restart
- * @document https://github.com/huangdijia/laravel-horizon-restart/blob/master/README.md
+ * @document https://github.com/huangdijia/laravel-horizon-restart/blob/2.x/README.md
  * @contact  huangdijia@gmail.com
  */
 $header = <<<'EOF'
-This file is part of huangdijia/laravel-horizon-restart.
+This file is part of laravel-horizon-restart.
 
 @link     https://github.com/huangdijia/laravel-horizon-restart
 @document https://github.com/huangdijia/laravel-horizon-restart/blob/2.x/README.md
--- src/Console/RestartCommand.php
+++ src/Console/RestartCommand.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 /**
- * This file is part of huangdijia/laravel-horizon-restart.
+ * This file is part of laravel-horizon-restart.
  *
  * @link     https://github.com/huangdijia/laravel-horizon-restart
  * @document https://github.com/huangdijia/laravel-horizon-restart/blob/2.x/README.md
