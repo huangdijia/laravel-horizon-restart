228b7b97f49a38e5b5419f085d39595cf3bc181d
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-08-16 09:28:58 +0800

    Update php coding standard

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
  * This file is part of laravel-horizon-restart.
  *
  * @link     https://github.com/huangdijia/laravel-horizon-restart
- * @document https://github.com/huangdijia/laravel-horizon-restart/blob/2.x/README.md
+ * @document https://github.com/huangdijia/laravel-horizon-restart/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
+
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
